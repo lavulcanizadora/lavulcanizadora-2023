a24e7cfd5bd02834c0a1397d99766c8cf6bf1817
neatdisorder <nachocast92@gmail.com>
2023-06-23 18:35:11 -0300

    Static generator para project

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -4,23 +4,44 @@ import fetchEntries from "@/util/contentfulQuery";
 import fetchAsset from "@/util/contentfulAsset";
 import ProjectInfo from "@/src/layouts/ProjectInfo";
 
-const Project = ({ logoUrl, projectInfo }) => {
+const Project = ({ logoUrl, projectInfo, paths }) => {
   return <ProjectInfo logoUrl={logoUrl} projectInfo={projectInfo[0]} />;
 };
 
 Project.propTypes = {
   projectInfo: PropTypes.array.isRequired,
-  logoUrl: PropTypes.string.isRequired
+  logoUrl: PropTypes.string.isRequired,
 };
 
-export const getStaticPaths = async() => {
+export const getStaticPaths = async () => {
+  const projectsListObject = await fetchEntries({
+    content_type: "project",
+  });
+
+  const projectsList = [];
+
+  projectsListObject.forEach((projectObject) =>
+    projectsList.push(projectObject.fields.slug)
+  );
+
+  const paths = [];
+
+  projectsList.forEach((project) =>
+    paths.push({ params: { project: project }, locale: "en" })
+  );
+
+  projectsList.forEach((project) =>
+    paths.push({ params: { project: project }, locale: "es" })
+  );
+
   return {
-    paths: [],
-    fallback: false
+    paths: paths,
+    fallback: true,
   };
 };
 
 export const getStaticProps = async (context) => {
+
   // Traer info del proyecto
 
   const res = await fetchEntries({
