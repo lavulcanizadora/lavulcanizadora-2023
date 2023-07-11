e53173e279bcf7eac334cc5c274921997976fc22
neatdisorder <nachocast92@gmail.com>
2023-07-11 12:54:34 -0300

    Fix de carousel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,24 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
+import AutoHeight from "embla-carousel-auto-height";
 import { Box, Flex, Image } from "@chakra-ui/react";
 import style from "./style";
 
 const Carousel = ({ carouselSlides }) => {
-  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [
+    Autoplay({ stopOnInteraction: false }),
+    AutoHeight(),
+  ]);
 
   return (
     <Box {...style.embla} className="embla">
       <Box className="embla__viewport" ref={emblaRef}>
         <Flex className="embla__container" {...style.emblaContainer}>
           {carouselSlides.map((slide) => (
-            <Flex
+            <Image
+              alt=""
+              src={slide.fields.file.url}
               {...style.emblaSlide}
               key={slide.fields.file.url}
               className="embla__slide"
-            >
-              <Image alt="" src={slide.fields.file.url} />
-            </Flex>
+              onLoad={() => {
+                if (emblaApi) emblaApi.reInit();
+              }}
+            />
           ))}
         </Flex>
       </Box>
