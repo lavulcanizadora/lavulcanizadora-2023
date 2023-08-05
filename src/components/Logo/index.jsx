33e60805995c83c49164244c4f640e2e3aea70b7
neatdisorder <nachocast92@gmail.com>
2023-08-05 17:56:35 -0300

    Cambio de link en logo

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -3,28 +3,28 @@ import PropTypes from "prop-types";
 import { Flex, Image, Hide } from "@chakra-ui/react";
 import style from "./style.js";
 import Link from "next/link.js";
-// import AddressParagraph from "../AddressParagraph/index.jsx";
 
-const Logo = ({ logoUrl, address, isHomepage }) => {
+const Logo = ({ logoUrl, isHomepage }) => {
+
   return (
     <Flex {...style.container}>
-      <Link href="/" passHref legacyBehavior>
+      <Link href={"/home"} passHref legacyBehavior>
         {isHomepage ? (
-          <Image src={logoUrl} alt="Logo La Vulcanizadora" {...style.logoInvert} />
+          <Image
+            src={logoUrl}
+            alt="Logo La Vulcanizadora"
+            {...style.logoInvert}
+          />
         ) : (
           <Image src={logoUrl} alt="Logo La Vulcanizadora" {...style.logo} />
         )}
       </Link>
-      {/* <Hide below="md">
-        <AddressParagraph isHomepage={isHomepage} address={address} />
-      </Hide> */}
     </Flex>
   );
 };
 
 Logo.propTypes = {
   logoUrl: PropTypes.string.isRequired,
-  address: PropTypes.object.isRequired,
   isHomepage: PropTypes.bool,
 };
 
